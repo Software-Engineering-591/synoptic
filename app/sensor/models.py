8144da2a5b5eea97613c579b1a8e5a5c5cdf4fa8
m4da <58071590+m4da@users.noreply.github.com>
2024-06-03 15:56:00 +0100

    Implemented Sensor model

--- app/sensor/models.py
+++ app/sensor/models.py
@@ -1,3 +1,13 @@
 from django.db import models  # noqa: F401
-
+from django.contrib.gis.db import models as geomodels
+import uuid
 # Create your models here.
+
+
+class Sensor(models.Model):
+    sensor_point = geomodels.PointField()
+    main_carstring = models.CharField(max_length=255)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.main_carstring
