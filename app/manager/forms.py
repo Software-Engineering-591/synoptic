4b3e0f390794be48d51998b901147ac5c9ebd3f8
TommyP2W <152071432+TommyP2W@users.noreply.github.com>
2024-06-04 22:03:46 +0100

    Improved Login Page

--- app/manager/forms.py
+++ app/manager/forms.py
@@ -6,11 +6,11 @@ from django.contrib.auth.forms import AuthenticationForm
 class login_form(AuthenticationForm):
     ## Fields for login form, seems for username, to enforce clientside, need a camal cased maxLength html attribute to enforce
     username = forms.CharField(
-        max_length = 10,
-        widget=forms.TextInput(attrs={'class': 'w-full caret-pink-500', 'placeholder' : 'username', 'maxLength' : '10'})
+        max_length = 50,
+        widget=forms.TextInput(attrs={'class': 'w-full caret-pink-500', 'placeholder' : 'username', 'maxLength' : '50'})
     )
     password = forms.CharField(
-        max_length= 10,
+        max_length= 50,
         widget=forms.PasswordInput(attrs={'class': 'caret-pink-500 w-full', 'placeholder' : 'password'})
     )
 
--- app/manager/views.py
+++ app/manager/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render, redirect  # noqa: F401
+from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from manager.forms import login_form
+from django.core.exceptions import ValidationError
+
 
 # Create your views here.
+def adminView(request):
+    if request.method == 'POST':
+        attempt = login_form(request, data=request.POST)
+        if attempt.is_valid():
+            username1 = attempt.cleaned_data['username']
+            password1 = attempt.cleaned_data['password']
+            user = authenticate(request, username=username1, password=password1)
+
+            if user is not None:
+                    login(request, user)
+                    return render(request, 'admin_login.html', {'form' : attempt})
+            else:
+                return render(request, 'admin_login.html' , {'form' : attempt})
+        else:
+            return render(request, 'admin_login.html', {'form' : attempt})
+    else:
+        attempt = login_form()
+        return render(request, 'admin_login.html', {'form' : attempt})
+
+    
