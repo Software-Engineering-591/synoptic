f661604015c377b361f0f079f4a963587e143bd3
Nitro Sniper <44097331+NitroSniper@users.noreply.github.com>
2024-06-07 21:05:10 +0100

    removed unused import

--- app/public/views.py
+++ app/public/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render  # noqa: F401
 from django.views.generic import TemplateView
-from django.utils.translation import gettext_lazy as _
 # Create your views here.
 
 
