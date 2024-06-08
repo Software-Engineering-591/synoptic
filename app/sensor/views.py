6e4005779cb28b3b1a95f766e69034d546a772e6
TommyP2W <152071432+TommyP2W@users.noreply.github.com>
2024-06-08 14:19:01 +0100

    Update sensor views.py
    removed this

--- app/sensor/views.py
+++ app/sensor/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render  # noqa: F401
-from .models import Sensor
+
 
 # Create your views here.
-def home(request):
-    sensors = Sensor.objects.all()
-    return render(request, 'home.html', {'sensors': sensors})
