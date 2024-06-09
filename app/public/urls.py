c6969eab8037576a0f8380f16a739e6cf6a35d85
Nitro Sniper <44097331+NitroSniper@users.noreply.github.com>
2024-06-09 15:14:45 +0100

    Prototype for alert page

--- app/public/urls.py
+++ app/public/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
+    path('alert/', views.AlertView.as_view()),
 ]
--- app/public/views.py
+++ app/public/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render  # noqa: F401
 from django.views.generic import TemplateView
 from manager.models import WaterReading
 from sensor.models import Sensor
+from .weatherapi import get_weather_data
 # Create your views here.
 
 
@@ -15,3 +16,22 @@ class HomeView(TemplateView):
                 for sensor in Sensor.objects.all()
             )
         }
+
+
+class AlertView(TemplateView):
+    template_name = 'public/alert.html'
+
+    def get_context_data(self):
+        # Debugging rn so no return
+        import os
+
+        lat = 12.629589810489191
+        lon = 106.95043710454692
+
+        appid = os.environ.get('WEATHERAPI_KEY')
+
+        m = get_weather_data(lat=lat, lon=lon, appid=appid)
+        if m.is_ok():
+            return {'weathers': m.list[:4], 'city': m.city}
+        print("Couldn't get weather data")
+        return {'weathers': None, 'city': None}
