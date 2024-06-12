2c529cd0f9aeb9392f0874edd3e6ec9629d2ee12
TommyP2W <152071432+TommyP2W@users.noreply.github.com>
2024-06-12 02:20:35 +0100

    Add sensor page + partial translations (#23)
    * Moved_files
    
    * small tweaks
    
    * tobefixed
    
    * Functional add water reading page, formatted, still need to refine but almost complete implementation
    
    * almostcompleteaddsensor
    
    * Add_sensor + anything else
    
    * integration
    
    * Integration
    
    * ruffruff
    
    * plsruffhavemercy
    
    * formatting and error issues
    
    * added logout functionality
    
    * Added authentication verification to dashboard, titles and fix map coords
    
    * minor changes
    
    * beforepull
    
    * Added partial translations
    
    * reformat
    
    * ruff

--- app/sensor/models.py
+++ app/sensor/models.py
@@ -6,7 +6,7 @@ import uuid
 
 class Sensor(models.Model):
     point = geomodels.PointField()
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
