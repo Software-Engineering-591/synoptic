25a3a20a1cac417a6a36251dcca1b19f9f526e6b
Nitro Sniper <44097331+NitroSniper@users.noreply.github.com>
2024-06-12 08:49:56 +0100

    Update urls.py

--- app/manager/urls.py
+++ app/manager/urls.py
@@ -3,7 +3,7 @@ from . import views  # noqa: F401
 
 urlpatterns = [
     path('login/', views.admin_view, name='login'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
     path('add_water/', views.add_water_view, name="add_water"),
     path('add_sensor/', views.add_sensor_view, name='add_sensor'),
     path('', views.dashboard, name='dashboard'),
